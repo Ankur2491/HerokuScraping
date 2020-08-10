c40d16f49f736fc3a205f5914169d7dc2cc1a851
ankur_sharma4 <ankur_sharma4@apple.com>
2020-08-11 03:58:25 +0530

    initial commit

---
+++ test/index.js
@@ -0,0 +1,76 @@
+const Cors = require('cors');
+var redis = require('redis');
+let redis_host = "redis-11422.c62.us-east-1-4.ec2.cloud.redislabs.com"
+let redis_port = 11422
+let redis_password = "AFahzbIs3wTxs0VMPnvTqkuqyoZOWXwV"
+var client = redis.createClient({ host: redis_host, port: redis_port, password: redis_password })
+const express = require("express");
+const app = express();
+app.use(Cors())
+let port = process.env.PORT || 3000;
+app.get("/all", (req, res) => {
+    client.get('all_news',(err, resp)=>{
+        let data = JSON.parse(resp);
+        res.send(data['news'][0]['articles']);
+    })
+});
+
+app.get("/general", (req, res) => {
+    client.get('all_news',(err, resp)=>{
+        let data = JSON.parse(resp);
+        res.send(data['news'][1]['articles']);
+    })
+});
+
+app.get("/business", (req, res) => {
+    client.get('all_news',(err, resp)=>{
+        let data = JSON.parse(resp);
+        res.send(data['news'][2]['articles']);
+    })
+});
+
+app.get("/entertainment", (req, res) => {
+    client.get('all_news',(err, resp)=>{
+        let data = JSON.parse(resp);
+        res.send(data['news'][3]['articles']);
+    })
+});
+
+app.get("/health", (req, res) => {
+    client.get('all_news',(err, resp)=>{
+        let data = JSON.parse(resp);
+        res.send(data['news'][4]['articles']);
+    })
+});
+
+app.get("/science", (req, res) => {
+    client.get('all_news',(err, resp)=>{
+        let data = JSON.parse(resp);
+        res.send(data['news'][5]['articles']);
+    })
+});
+
+app.get("/technology", (req, res) => {
+    client.get('all_news',(err, resp)=>{
+        let data = JSON.parse(resp);
+        res.send(data['news'][6]['articles']);
+    })
+});
+
+app.get("/sport", (req, res) => {
+    client.get('all_news',(err, resp)=>{
+        let data = JSON.parse(resp);
+        res.send(data['news'][7]['articles']);
+    })
+});
+
+app.get("/offbeat", (req, res) => {
+    client.get('all_news',(err, resp)=>{
+        let data = JSON.parse(resp);
+        res.send(data['news'][8]['articles']);
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Example app is listening on port http://localhost:${port}`)
+});
